59c12b93b41fde12142460b2fe4e36bc732aec56
kchoon <kchoon@users.noreply.github.com>
2020-03-23 20:07:06 +0900

    200323

---
+++ assets/js/swiper_.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper2', {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        freeMode: true,
+//        centeredSlides: true,
+        loop:false,
+    })        
+});
+
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper1', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+//        centeredSlides: true,
+        loop:false,
+    })        
+});
+
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper3', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+//        centeredSlides: true,
+        loop:false,
+    })        
+});
